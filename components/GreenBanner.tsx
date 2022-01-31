df980750ba5a5d2c06429022b66f6e3137479046
Sabari2810 <imsabarimanikandan@gmail.com>
2022-01-31 22:40:30 +0530

    xs breakpoint to menu

--- components/GreenBanner.tsx
+++ components/GreenBanner.tsx
@@ -99,24 +99,27 @@ const GreenBanner = () => {
                     <hr className='my-7 border-y-1 border-green-400 w-full' />
                 </div>
             </div>
-            <p id='gbDescr' className={`max-w-4xl px-20 lg:px-0 font-serif text-black-dark opacity-95 font-thin text-center ${isGbDescrInViewport && 'animate-slideUp duration-1000'}
-         leading-relaxed text-2xl`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
-            <div id='gbStats' className={`py-16 grid grid-cols-2 space-x-4 lg:space-x-0 lg:grid-cols-4 items-center ${isGbStatsInViewport && 'animate-slideUp duration-1000'}`}>
-                <div className='flex flex-col items-center px-14 mb-8 lg:mb-0 border-r space-y-4 border-green-400 pb-2'>
+            <p id='gbDescr' className={`max-w-4xl px-8 sm:px-20 lg:px-0 font-serif text-black-dark opacity-95 font-thin text-center ${isGbDescrInViewport && 'animate-slideUp duration-1000'}
+         leading-relaxed text-lg sm:text-2xl`}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</p>
+            <div id='gbStats' className={`py-16 grid gap-6 sm:grid-cols-2 sm:space-x-4 lg:space-x-0 lg:grid-cols-4 items-center ${isGbStatsInViewport && 'animate-slideUp duration-1000'}`}>
+                <div className='flex flex-col items-center px-14 sm:mb-8 lg:mb-0 sm:border-r space-y-4 border-green-400 pb-2'>
                     <p className='text-7xl text-white font-bold duration-100 transition-all'>{
                         awards
                     }</p>
                     <p className='text-xl font-bold capitalize'>Awards Received</p>
                 </div>
-                <div className='flex flex-col items-center px-14 mb-8 lg:mb-0 lg:border-r space-y-4 border-green-400 pb-2'>
+                <hr className='sm:hidden w-full max-w-[16rem] mx-auto border-green-500' />
+                <div className='flex flex-col items-center px-14 sm:mb-8 lg:mb-0 lg:border-r space-y-4 border-green-400 pb-2'>
                     <p className='text-7xl font-sans text-white font-bold'>{cups}</p>
                     <p className='text-xl font-bold capitalize'>cups of coffee</p>
                 </div>
-                <div className='flex flex-col items-center px-14 border-r space-y-4 border-green-400 pb-2'>
+                <hr className='sm:hidden w-full max-w-[16rem] mx-auto border-green-500' />
+                <div className='flex flex-col items-center px-14 sm:border-r space-y-4 sm:border-green-400 pb-2'>
                     <p className='text-7xl text-white font-bold'>{projects}</p>
                     <p className='text-xl font-bold capitalize'>projects completed</p>
                 </div>
-                <div className='flex flex-col items-center px-14 border-white space-y-4 pb-2'>
+                <hr className='sm:hidden w-full max-w-[16rem] mx-auto border-green-500' />
+                <div className='flex flex-col items-center px-14 sm:border-white space-y-4 pb-2'>
                     <p className='text-7xl text-white font-bold'>{clients}</p>
                     <p className='text-xl font-bold capitalize'>happy clients</p>
                 </div>
--- components/Services.tsx
+++ components/Services.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { FaAddressBook, FaBrush } from 'react-icons/fa';
-import { AiFillBuild, AiFillFormatPainter } from 'react-icons/ai';
+import { FaAddressBook, FaBrush, FaMarsStrokeV, FaSpeakerDeck } from 'react-icons/fa';
+import { AiFillBuild, AiFillFormatPainter, AiFillMinusCircle, AiFillRocket, AiFillStepBackward, AiOutlineGlobal } from 'react-icons/ai';
 
 const Services = () => {
     const [isServicesHeaderInViewPort, setIsServicesHeaderInViewPort] = useState(false);
@@ -45,39 +45,43 @@ const Services = () => {
         <div id='serviceHeader' className={`flex flex-col items-center ${isServicesHeaderInViewPort && 'animate-slideUp duration-1000'}`}>
             <h2 className='sec-header-title'>what we do</h2>
             <h3 className='sec-header-descr text-black-dark'>We’ve got everything you need to launch and grow your business</h3>
-            <hr className='border-slate-300 max-w-xl w-full my-4' />
+            <hr className='border-slate-300 max-w-[12rem] sm:max-w-xl w-full my-4' />
         </div>
         <div id='service1' className={`grid gap-10 lg:grid-cols-2 grid-cols-1 ${isServiceOneInViewPort && 'animate-slideUp duration-1000'}
          items-center space-y-7 lg:space-x-16 max-w-2xl lg:max-w-7xl mx-auto`}>
-            <div className='flex flex-col lg:space-y-0 space-y-8 lg:flex-row items-center lg:items-start justify-start space-x-5'>
+            <div className='flex flex-col lg:space-y-0 space-y-8 lg:flex-row items-center lg:items-start
+             justify-start sm:space-x-5'>
                 <AiFillFormatPainter className='w-52 h-20 text-green-500' />
                 <div className='flex lg:items-start lg:space-y-0 space-y-6 items-center flex-col'>
                     <h3 className='text-3xl pb-3 font-semibold capitalize'>brand identity</h3>
-                    <p className='text-slate-500 max-w-md leading-relaxed text-center lg:text-left'>Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
+                    <p className='text-slate-500 px-4 sm:px-0 max-w-md leading-relaxed text-center lg:text-left'>Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
                 </div>
             </div>
-            <div className='flex flex-col lg:flex-row lg:space-y-0 space-y-8 items-center lg:items-start justify-start space-x-5'>
+            <div className='flex flex-col lg:flex-row lg:space-y-0 space-y-8 items-center lg:items-start 
+            justify-start sm:space-x-5'>
                 <AiFillBuild className='w-52 h-20 text-green-500' />
                 <div className='flex lg:items-start lg:space-y-0 space-y-6 items-center flex-col'>
                     <h3 className='text-3xl pb-3 font-semibold capitalize'>Illustration</h3>
-                    <p className='text-slate-500 max-w-md leading-relaxed text-center lg:text-left'>Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
+                    <p className='text-slate-500 max-w-md px-4 sm:px-0 leading-relaxed text-center lg:text-left'>Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
                 </div>
             </div>
         </div>
         <div id='service2' className={`grid gap-10 lg:grid-cols-2 grid-cols-1 ${isServiceTwoInViewPort && 'animate-slideUp duration-1000'}
          items-center space-y-7 lg:space-x-16 max-w-2xl lg:max-w-7xl mx-auto`}>
-            <div className='flex flex-col lg:space-y-0 space-y-8 lg:flex-row items-center lg:items-start justify-start space-x-5'>
-                <AiFillFormatPainter className='w-52 h-20 text-green-500' />
-                <div className='flex lg:items-start lg:space-y-0 space-y-6 items-center flex-col'>
-                    <h3 className='text-3xl pb-3 font-semibold capitalize'>brand identity</h3>
-                    <p className='text-slate-500 max-w-md leading-relaxed text-center lg:text-left'>Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
+            <div className='flex flex-col lg:space-y-0 space-y-8 lg:flex-row items-center lg:items-start 
+            justify-start sm:space-x-5'>
+                <AiFillRocket className='w-52 h-20 text-green-500' />
+                <div className='flex lg:items-start lg:space-y-0 sm:space-y-6 items-center flex-col'>
+                    <h3 className='text-3xl pb-3 font-semibold capitalize'>Marketing</h3>
+                    <p className='text-slate-500 max-w-md px-4 sm:px-0  leading-relaxed text-center lg:text-left'>Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
                 </div>
             </div>
-            <div className='flex flex-col lg:flex-row lg:space-y-0 space-y-8 items-center lg:items-start justify-start space-x-5'>
-                <AiFillBuild className='w-52 h-20 text-green-500' />
+            <div className='flex flex-col lg:flex-row lg:space-y-0 space-y-8 items-center lg:items-start justify-start
+             sm:space-x-5'>
+                <AiOutlineGlobal className='w-52 h-20 text-green-500' />
                 <div className='flex lg:items-start lg:space-y-0 space-y-6 items-center flex-col'>
-                    <h3 className='text-3xl pb-3 font-semibold capitalize'>Illustration</h3>
-                    <p className='text-slate-500 max-w-md leading-relaxed text-center lg:text-left'>Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
+                    <h3 className='text-3xl pb-3 font-semibold capitalize'>Web Design</h3>
+                    <p className='text-slate-500 max-w-md px-4 sm:px-0 leading-relaxed text-center lg:text-left'>Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.</p>
                 </div>
             </div>
         </div>
